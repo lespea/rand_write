cf8324b38fcbc8fad13741417b4ac44ad1a0c305
Adam Lesperance <lespea@gmail.com>
2019-11-15 15:56:40 -0600

    Impl writing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::path::PathBuf;
-use std::thread;
+use std::time::{Duration, Instant};
 
 use crossbeam::thread::scope;
 use fs2;
 use humantime::{format_duration, FormattedDuration};
 use indicatif::ProgressBar;
 use indicatif::{MultiProgress, ProgressStyle};
-use std::time::{Duration, Instant};
+use rand::prelude::*;
+use rand::thread_rng;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -30,26 +33,44 @@ fn main() {
         let multi = MultiProgress::new();
 
         for p in opt.paths.into_iter() {
-            let start = Instant::now();
-            let prog_bar = ProgressBar::new(fs2::free_space(&p).unwrap());
+            let prog_bar = ProgressBar::new(fs2::free_space(&p).expect(&format!("Couldn't get the total space for {}", p.display())));
 
             prog_bar.set_style(ProgressStyle::default_bar().template(
-                "[{elapsed_precise}] {bar:40.cyan/blue} {bytes:>7}/{total_bytes:7} :: {eta_precise} {msg}",
+                "[{elapsed_precise}] {bar:40.cyan/blue} {bytes:>7}/{total_bytes:7} => {bytes_per_sec} :: {eta_precise} {msg}",
             ));
             multi.add(prog_bar.clone());
 
+            let mut fh = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .truncate(true)
+                .open(&p).expect(&format!("Couldn't open {}", p.display()));
+
             prog_bar.set_message(&format!("{}", p.display()));
             s.spawn(move |_| {
-                for _ in 0..10 {
-                    prog_bar.inc(1 << 25);
-                    thread::sleep(Duration::from_secs(1));
+                let start = Instant::now();
+
+                let rng = thread_rng();
+                let mut chacha = rand_chacha::ChaCha12Rng::from_rng(rng).unwrap();
+                let mut buf = [0u8; 1 << 19];
+
+                loop {
+                    chacha.fill_bytes(&mut buf);
+                    match fh.write(&mut buf) {
+                        Ok(l) => prog_bar.inc(l as u64),
+                        Err(e) => {
+                            prog_bar.println(&format!("Error writing to {}: {}", p.display(), e));
+                            break;
+                        }
+                    }
                 }
-                //                bar.finish_at_current_pos();
+
                 prog_bar.println(format!(
                     "Finished {} after {}",
                     p.display(),
                     to_dur(start),
                 ));
+
                 prog_bar.finish_and_clear();
             });
         }
