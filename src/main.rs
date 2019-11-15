3e21dcc08e81f75b5f68c607f6ca4698d3ca2617
Adam Lesperance <lespea@gmail.com>
2019-11-15 17:58:20 -0600

    Linux optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
 use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
@@ -45,12 +45,38 @@ fn freespace(path: &Path) -> u64 {
     fs2::free_space(&p).expect(&format!("Couldn't get the total space for {}", p.display()))
 }
 
+#[cfg(target_os = "linux")]
+fn open(p: &Path) -> File {
+    use std::os::unix::fs::OpenOptionsExt;
+
+    OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .custom_flags(libc::O_DIRECT)
+        .open(&p)
+        .unwrap_or_else(|_| panic!("Couldn't open {}", p.display()))
+}
+
+#[cfg(target_os = "windows")]
+fn open(path: &Path) -> File {
+    OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(&p)
+        .unwrap_or_else(|_| panic!("Couldn't open {}", p.display()))
+}
+
 fn to_dur(start: Instant) -> FormattedDuration {
     let el = start.elapsed();
     format_duration(
         Duration::from_secs(el.as_secs()) + Duration::from_millis(el.subsec_millis() as u64),
     )
 }
+
+#[repr(align(512))]
+struct Buf([u8; 1 << 19]);
 
 fn main() {
     let opt = Opt::from_args();
@@ -59,6 +85,8 @@ fn main() {
         let multi = MultiProgress::new();
 
         for p in opt.paths.into_iter() {
+            let mut fh = open(&p);
+
             let prog_bar = ProgressBar::new(freespace(&p));
 
             prog_bar.set_style(ProgressStyle::default_bar().template(
@@ -66,24 +94,18 @@ fn main() {
             ));
             multi.add(prog_bar.clone());
 
-            let mut fh = OpenOptions::new()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .open(&p)
-                .unwrap_or_else(|_| panic!("Couldn't open {}", p.display()));
-
             prog_bar.set_message(&format!("{}", p.display()));
             s.spawn(move |_| {
                 let start = Instant::now();
 
                 let rng = thread_rng();
                 let mut chacha = rand_chacha::ChaCha12Rng::from_rng(rng).unwrap();
-                let mut buf = [0u8; 1 << 19];
+
+                let mut buf = Buf([0u8; 1 << 19]);
 
                 loop {
-                    chacha.fill_bytes(&mut buf);
-                    match fh.write(&buf) {
+                    chacha.fill_bytes(&mut buf.0);
+                    match fh.write(&buf.0) {
                         Ok(l) => prog_bar.inc(l as u64),
                         Err(e) => {
                             prog_bar.println(&format!("Error writing to {}: {}", p.display(), e));
